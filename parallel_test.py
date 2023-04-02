4e07dd5913979b552255c7a39bb3b399ebb4dbdc
Abhinandan Udupa <67597710+abhinandanudupa@users.noreply.github.com>
2023-04-02 19:43:58 +0530

    Limit maximum no. of concurrent running threads

--- parallel_test.py
+++ parallel_test.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+from multiprocessing.dummy import Pool as ThreadPool
 import os
 import sys
 import threading
@@ -7,14 +8,14 @@ import fnmatch
 
 
 MYTHRIL = (
-    "time -v myth a {} --parallel-solving --execution-timeout 600 -o json > {} 2>&1"
+    "time -v myth a {} --parallel-solving --execution-timeout 6 -o json > {} 2>&1"
 )
 OYENTE = "time -v oyente -s {} > {} 2>&1"
 TOOL = "MYTHRIL"
+MAX_NO_OF_THREADS = 7
 
 
 result_path_root = os.path.expanduser("~/results")
-spawned_threads = []
 
 
 def analyse_contract(contract_file_full_path, category_name):
@@ -23,7 +24,7 @@ def analyse_contract(contract_file_full_path, category_name):
 
     contract_file = os.path.basename(contract_file_full_path)
     print(
-        "{} > Analysing {}.....".format(threading.current_thread().name, contract_file)
+        "{}>> Analysing {}.....".format(threading.current_thread().name, contract_file)
     )
 
     if TOOL == "MYTHRIL":
@@ -39,7 +40,7 @@ def analyse_contract(contract_file_full_path, category_name):
         os.system(OYENTE.format(contract_file_full_path, text_result_file))
 
     print(
-        "{} > Finished analysing {}".format(
+        "{}>> Finished analysing {}".format(
             threading.current_thread().name, contract_file
         )
     )
@@ -52,6 +53,7 @@ def analyse_contracts_in_folder(full_path_to_category_folder):
 
 
 def walk_through_all_categories(dataset_root):
+    folders = []
     for folder in os.listdir(dataset_root):
         target_folder_path = os.path.join(dataset_root, folder)
 
@@ -59,22 +61,27 @@ def walk_through_all_categories(dataset_root):
             result_folder = os.path.join(result_path_root, folder)
             if not os.path.exists(result_folder):
                 os.makedirs(result_folder)
+            folders.append(target_folder_path)
 
-            print("% Starting thread for {} ....".format(folder))
-            spawned_threads.append(
-                threading.Thread(
-                    target=analyse_contracts_in_folder,
-                    args=(target_folder_path,),
-                    name="({})".format(folder),
-                )
-            )
-            spawned_threads[-1].start()
+    pool = ThreadPool(MAX_NO_OF_THREADS)
+    pool.map(analyse_contracts_in_folder, folders)
+    pool.close()
+    pool.join()
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         print("+" * 60)
         print("No dataset folder specified!")
+        print(
+            """
+Usage: ./script.py <contracts_folder> [<results_folder>] [<no_of_threads>]
+
+contracts_folder: specifies the folder where the contracts of different types are present in the respective sub folders.
+results_folder: an optional argument which specifies the folder where the results will be stored, defaults to ~/results/
+no_of_threads: specifies the maximum number of threads running currently at any time, defaults to 7
+"""
+        )
         exit()
 
     if os.path.exists(sys.argv[1]):
@@ -86,15 +93,16 @@ if __name__ == "__main__":
         if not os.path.exists(result_path_root):
             os.makedirs(result_path_root)
 
+        if len(sys.argv) == 4:
+            MAX_NO_OF_THREADS = int(sys.argv[3])
+
         print("+" * 60, end="\n\n")
         print("Starting analysis using", TOOL)
-        print("The results will be stored in:", result_path_root, end="\n\n")
+        print(f"The results will be stored in:", result_path_root)
+        print("The maximum number of threads will be limited to:", MAX_NO_OF_THREADS, end="\n\n")
         print("+" * 60, end="\n\n")
 
         walk_through_all_categories(dataset_abs_path)
-
-        for thread in spawned_threads:
-            thread.join()
 
         print()
         print("+" * 60, end="\n\n")
