08c3fbbf88b24173b4cc24e8d7b335d33de85701
Abhinandan <67597710+abhinandanudupa@users.noreply.github.com>
2023-06-12 11:47:55 +0530

    Refractor

--- parallel_test.py
+++ parallel_test.py
@@ -1,3 +1,6 @@
+"""Script to run the tools - mythril and oyente in parallel on a dataset and collect the results"""
+
+
 #! /usr/bin/env python3
 
 from multiprocessing.dummy import Pool as ThreadPool
@@ -7,9 +10,7 @@ import threading
 import fnmatch
 
 
-MYTHRIL = (
-    "time -v myth a {} --parallel-solving --execution-timeout 6 -o json > {} 2>&1"
-)
+MYTHRIL = "time -v myth a {} --parallel-solving --execution-timeout 6 -o json > {} 2>&1"
 OYENTE = "time -v /oyente/oyente/oyente.py -s {} > {} 2>&1"
 TOOL = "MYTHRIL"
 MAX_NO_OF_THREADS = 7
@@ -19,40 +20,63 @@ result_path_root = os.path.expanduser("~/results")
 
 
 def analyse_contract(contract_file_full_path, category_name):
+    """Analyse the contract and find vulnerabilities.
+
+    Args:
+        contract_file_full_path: full path of the contract file to be ananlysed.
+        category_name: category of vulnerability the contract is suspected to have.
+    Returns:
+        None.
+
+    """
     if not fnmatch.fnmatch(contract_file_full_path, "*.sol"):
         return
 
     contract_file = os.path.basename(contract_file_full_path)
-    print(
-        "{}>> Analysing {}.....".format(threading.current_thread().name, contract_file)
-    )
+    print(f"{threading.current_thread().name}>> Analysing {contract_file}.....")
 
     if TOOL == "MYTHRIL":
         json_result_file = os.path.join(
-            result_path_root, category_name, str(contract_file).replace(".sol", ".json")
+            result_path_root,
+            category_name,
+            str(contract_file).replace(".sol", ".json"),
         )
         os.system(MYTHRIL.format(contract_file_full_path, json_result_file))
 
     else:
         text_result_file = os.path.join(
-            result_path_root, category_name, str(contract_file).replace(".sol", ".txt")
+            result_path_root,
+            category_name,
+            str(contract_file).replace(".sol", ".txt"),
         )
         os.system(OYENTE.format(contract_file_full_path, text_result_file))
 
-    print(
-        "{}>> Finished analysing {}".format(
-            threading.current_thread().name, contract_file
-        )
-    )
+    print(f"{threading.current_thread().name}>> Finished analysing {contract_file}")
 
 
 def analyse_contracts_in_folder(full_path_to_category_folder):
+    """Finds all contracts in a folder and calls a method to analyse them.
+
+    Args:
+        full_path_to_category_folder: full path to the folder containing smart contracts.
+    Returns:
+        None.
+
+    """
     for contract in os.listdir(full_path_to_category_folder):
         full_path = os.path.join(full_path_to_category_folder, contract)
         analyse_contract(full_path, os.path.split(full_path_to_category_folder)[-1])
 
 
 def walk_through_all_categories(dataset_root):
+    """Finds all folders inside the given root folder.
+
+    Args:
+        dataset_root: full path to the folder containing folders which may contain smart contracts.
+    Returns:
+        None.
+
+    """
     folders = []
     for folder in os.listdir(dataset_root):
         target_folder_path = os.path.join(dataset_root, folder)
@@ -77,12 +101,16 @@ if __name__ == "__main__":
             """
 Usage: ./script.py <contracts_folder> [<results_folder>] [<no_of_threads>]
 
-contracts_folder: specifies the folder where the contracts of different types are present in the respective sub folders.
-results_folder: an optional argument which specifies the folder where the results will be stored, defaults to ~/results/
-no_of_threads: specifies the maximum number of threads running currently at any time, defaults to 7
+contracts_folder:
+    specifies the folder where vulnerable contracts are present in the respective sub folders.
+results_folder [optional]:
+    specifies the folder where the results will be stored, defaults to ~/results/
+no_of_threads:
+    specifies the maximum number of threads running currently at any time, defaults to 7
+
 """
         )
-        exit()
+        sys.exit()
 
     if os.path.exists(sys.argv[1]):
         dataset_abs_path = os.path.abspath(sys.argv[1])
@@ -98,8 +126,11 @@ no_of_threads: specifies the maximum number of threads running currently at any 
 
         print("+" * 60, end="\n\n")
         print("Starting analysis using", TOOL)
-        print(f"The results will be stored in:", result_path_root)
-        print("The maximum number of threads will be limited to:", MAX_NO_OF_THREADS, end="\n\n")
+        print(f"The results will be stored in: {result_path_root}")
+        print(
+            f"The maximum number of threads will be limited to: {MAX_NO_OF_THREADS}",
+            end="\n\n",
+        )
         print("+" * 60, end="\n\n")
 
         walk_through_all_categories(dataset_abs_path)
