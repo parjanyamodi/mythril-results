f0f5945c1c06fb88ae69c414be2ff57ebc3f3c4f
Abhinandan Udupa <67597710+abhinandanudupa@users.noreply.github.com>
2023-08-12 17:34:24 +0530

    Update script to using remappings

--- parallel_test_single_folder.py
+++ parallel_test_single_folder.py
@@ -9,16 +9,17 @@ import json
 
 
 MYTHRIL = (
-    "command time -v myth a {} --parallel-solving --execution-timeout 60 -o json > {} 2>&1"
+    "command time -v myth a {} --parallel-solving --solc-json {} --execution-timeout 60 -o json > {} 2>&1"
 )
 MAX_NO_OF_THREADS = 1
 CONTRACTS_PER_THREAD = 10000
 
 
 result_path_root = os.path.expanduser("~/results")
+solc_json = "./solc_json.json"
 
 
-def analyse_contract(contract_file_full_path):
+def analyse_contract(contract_file_full_path, solc_json_file=solc_json):
     if not fnmatch.fnmatch(contract_file_full_path, "*.sol"):
         return
 
@@ -30,7 +31,7 @@ def analyse_contract(contract_file_full_path):
     json_result_file = os.path.join(
         result_path_root, str(contract_file).replace(".sol", ".json")
     )
-    os.system(MYTHRIL.format(contract_file_full_path, json_result_file))
+    os.system(MYTHRIL.format(contract_file_full_path, solc_json_file, json_result_file))
 
     print(
         "{}>> Finished analysing {}".format(
@@ -50,9 +51,10 @@ if __name__ == "__main__":
         print("No dataset folder specified!")
         print(
             """
-Usage: ./script.py <contracts_folder> [<results_folder>] [<no_of_threads>]
+Usage: ./script.py <contracts_folder> [<solc_json_file>] [<results_folder>] [<no_of_threads>]
 
 contracts_folder: specifies the folder where the contracts of different types are present.
+solc_json_file: specifies the solc config file which defines compiler configuration like remappings
 results_folder: an optional argument which specifies the folder where the results will be stored, defaults to ~/results/
 no_of_threads: specifies the maximum number of threads running currently at any time, defaults to 7
 """
@@ -62,14 +64,17 @@ no_of_threads: specifies the maximum number of threads running currently at any 
     if os.path.exists(sys.argv[1]):
         dataset_abs_path = os.path.abspath(sys.argv[1])
 
-        if len(sys.argv) == 3:
-            result_path_root = os.path.expanduser(sys.argv[2])
+        if len(sys.argv) >= 3:
+            solc_json = os.path.abspath(sys.argv[2])
+
+        if len(sys.argv) == 4:
+            result_path_root = os.path.expanduser(sys.argv[3])
 
         if not os.path.exists(result_path_root):
             os.makedirs(result_path_root)
 
-        if len(sys.argv) == 4:
-            MAX_NO_OF_THREADS = int(sys.argv[3])
+        if len(sys.argv) == 5:
+            MAX_NO_OF_THREADS = int(sys.argv[4])
 
         print("+" * 60, end="\n\n")
         print(f"The results will be stored in:", result_path_root)
